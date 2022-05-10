c80d9a92bd4d6f8e40e525f25182991e9abc77fe
tsunenaga-y <tsunenaga.yamada@gmail.com>
2022-05-10 17:02:25 +0900

    change limit of json body

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.json({ limit: '30mb' }));
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
